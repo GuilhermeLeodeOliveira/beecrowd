9e1873b68d438842df66cd4a1dffa6f03722302f
Guilherme Leo de Oliveira <97077427+GuilhermeLeodeOliveira@users.noreply.github.com>
2023-03-26 14:08:30 -0300

    Create 1759.java

---
+++ iniciante/java/1759.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner (System.in);
+
+		int x=sc.nextInt();
+		
+		for(int i=1; i<x; i++) {
+			System.out.print("Ho ");
+		}
+		
+		System.out.println("Ho!");
+		
+	}
+}
